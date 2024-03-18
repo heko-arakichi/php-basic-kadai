9c7dfc23eec94ccd46c01b8df221764f0fd72f1a
heko-arakichi <162960448+heko-arakichi@users.noreply.github.com>
2024-03-18 11:20:09 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vegetable_date = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($vegetable_date);
+        ?>
+    </p>
+</body>
+
+</html>
